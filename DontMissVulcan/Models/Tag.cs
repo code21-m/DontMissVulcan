989c9cb16adfadf24c0b73d328d45b4d457daa06
code21-m <raspberryjam7modely@gmail.com>
2025-10-19 00:35:16 +0900

    TagCategoryを別ファイルに移し、Qualificationの型を修正

--- DontMissVulcan/Models/Tag.cs
+++ DontMissVulcan/Models/Tag.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DontMissVulcan.Models
@@ -46,50 +44,5 @@ namespace DontMissVulcan.Models
 		Survival,       // 生存
 		Starter,        // 初期
 		Robot           // ロボット
-	}
-
-	internal static class TagCategory
-	{
-		public static readonly IImmutableSet<Tag> Qualification = [
-			Tag.SeniorOperator,
-			Tag.TopOperator
-		];
-
-		public static readonly ImmutableHashSet<Tag> Class = [
-			Tag.Vanguard,
-			Tag.Guard,
-			Tag.Defender,
-			Tag.Sniper,
-			Tag.Caster,
-			Tag.Medic,
-			Tag.Supporter,
-			Tag.Specialist
-		];
-
-		public static readonly ImmutableHashSet<Tag> Position = [
-			Tag.Melee,
-			Tag.Ranged
-		];
-
-		public static readonly ImmutableHashSet<Tag> Specialization =
-		[
-			Tag.AoE,
-			Tag.CrowdControl,
-			Tag.DPS,
-			Tag.DPRecovery,
-			Tag.Debuff,
-			Tag.FastRedeploy,
-			Tag.Defense,
-			Tag.Elemental,
-			Tag.Healing,
-			Tag.Nuker,
-			Tag.Shift,
-			Tag.Slow,
-			Tag.Summon,
-			Tag.Support,
-			Tag.Survival,
-			Tag.Starter,
-			Tag.Robot
-		];
 	}
 }
---
+++ DontMissVulcan/Models/TagCategory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontMissVulcan.Models
+{
+	internal static class TagCategory
+	{
+		public static readonly ImmutableHashSet<Tag> Qualification = [
+			Tag.SeniorOperator,
+			Tag.TopOperator
+		];
+
+		public static readonly ImmutableHashSet<Tag> Class = [
+			Tag.Vanguard,
+			Tag.Guard,
+			Tag.Defender,
+			Tag.Sniper,
+			Tag.Caster,
+			Tag.Medic,
+			Tag.Supporter,
+			Tag.Specialist
+		];
+
+		public static readonly ImmutableHashSet<Tag> Position = [
+			Tag.Melee,
+			Tag.Ranged
+		];
+
+		public static readonly ImmutableHashSet<Tag> Specialization =
+		[
+			Tag.AoE,
+			Tag.CrowdControl,
+			Tag.DPS,
+			Tag.DPRecovery,
+			Tag.Debuff,
+			Tag.FastRedeploy,
+			Tag.Defense,
+			Tag.Elemental,
+			Tag.Healing,
+			Tag.Nuker,
+			Tag.Shift,
+			Tag.Slow,
+			Tag.Summon,
+			Tag.Support,
+			Tag.Survival,
+			Tag.Starter,
+			Tag.Robot
+		];
+	}
+}
