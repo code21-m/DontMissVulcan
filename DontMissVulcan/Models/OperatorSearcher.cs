787317ed30fbaabb6220dfb29d64deade55134cd
code21-m <raspberryjam7modely@gmail.com>
2025-10-18 16:43:25 +0900

    `SearchEmployableOperatorsFromTags` のロジックをリファクタリングし、タグごとのフィルタリング処理を統一

--- DontMissVulcan/Models/OperatorSearcher.cs
+++ DontMissVulcan/Models/OperatorSearcher.cs
@@ -13,40 +13,48 @@ namespace DontMissVulcan.Models
 		public List<Operator> SearchEmployableOperatorsFromTags(List<string> tags)
 		{
 			var employableOperators = gameData.Operators.AsEnumerable();
-			if (tags.Contains("上級エリート"))
-			{
-				employableOperators = employableOperators.Where(o => o.Rarity == 6);
-				tags.Remove("上級エリート");
-			}
-			else if (tags.Contains("エリート"))
-			{
-				employableOperators = employableOperators.Where(o => o.Rarity == 5);
-				tags.Remove("エリート");
-			}
-			else
+			if (!tags.Contains("上級エリート"))
 			{
 				employableOperators = employableOperators.Where(o => o.Rarity <= 5);
 			}
-				foreach (var tag in tags)
+			foreach (var tag in tags)
+			{
+				if (gameData.Tags.Rarity.Contains(tag))
 				{
-					if (gameData.Tags.Job.Contains(tag))
-					{
-						employableOperators = employableOperators.Where(o => o.Job == JobTagToJob(tag));
-					}
-					else if (gameData.Tags.Position.Contains(tag))
-					{
-						employableOperators = employableOperators.Where(o => o.Position == tag);
-					}
-					else if (gameData.Tags.Other.Contains(tag))
-					{
-						employableOperators = employableOperators.Where(o => o.Tags.Contains(tag));
-					}
-					else
-					{
-						throw new ArgumentException($"タグ '{tag}' は有効なタグではありません。", nameof(tags));
-					}
+					employableOperators = employableOperators.Where(o => o.Rarity == RarityTagToRarity(tag));
 				}
+				else if (gameData.Tags.Job.Contains(tag))
+				{
+					employableOperators = employableOperators.Where(o => o.Job == JobTagToJob(tag));
+				}
+				else if (gameData.Tags.Position.Contains(tag))
+				{
+					employableOperators = employableOperators.Where(o => o.Position == tag);
+				}
+				else if (gameData.Tags.Other.Contains(tag))
+				{
+					employableOperators = employableOperators.Where(o => o.Tags.Contains(tag));
+				}
+				else
+				{
+					throw new ArgumentException($"タグ '{tag}' は有効なタグではありません。", nameof(tags));
+				}
+			}
 			return [.. employableOperators];
+		}
+
+		private int RarityTagToRarity(string rarityTag)
+		{
+			if (!gameData.Tags.Rarity.Contains(rarityTag))
+			{
+				throw new ArgumentException($"rarityTag '{rarityTag}' は有効なレア度タグではありません。", nameof(rarityTag));
+			}
+			return rarityTag switch
+			{
+				"上級エリート" => 6,
+				"エリート" => 5,
+				_ => 0,
+			};
 		}
 
 		private string JobTagToJob(string jobTag)
