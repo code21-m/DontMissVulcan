6096b16267dc95ca54540bd01c2e6ba662abdb78
code21-m <raspberryjam7modely@gmail.com>
2025-10-18 22:36:53 +0900

    タグの組み合わせと採用可能オペレーターを列挙する機能を実装 命名変更等のリファクタリング

--- DontMissVulcan/Models/OperatorFinder.cs
+++ DontMissVulcan/Models/OperatorFinder.cs
@@ -6,22 +6,35 @@ using System.Threading.Tasks;
 
 namespace DontMissVulcan.Models
 {
-	internal class OperatorSearcher(GameData gameData)
+	internal class OperatorFinder(GameData gameData)
 	{
 		private readonly GameData gameData = gameData;
+		private const int maximumSelectionsNumber = 3;
 
-		public IEnumerable<Operator> SearchEmployableOperatorsFromTags(IEnumerable<Tag> tags)
+		public IEnumerable<(IEnumerable<Tag> tags, IEnumerable<Operator> operators)> EnumerateOperatorsForTagsCombinations(IEnumerable<Tag> appearedTags)
+		{
+			for(var r = 1; r <= maximumSelectionsNumber; r++)
+			{
+				foreach(var selectedTags in appearedTags.EnumerateCombinations(r))
+				{
+					var result= GetOperatorsByTags(selectedTags);
+					yield return (selectedTags, result);
+				}
+			}
+		}
+
+		public IEnumerable<Operator> GetOperatorsByTags(IEnumerable<Tag> selectedTags)
 		{
 			var employableOperators = gameData.Operators.AsEnumerable();
-			if (!tags.Contains(Tag.SeniorOperator))
+			if (!selectedTags.Contains(Tag.SeniorOperator))
 			{
 				employableOperators = employableOperators.Where(o => o.Rarity <= 5);
 			}
-			foreach (var tag in tags)
+			foreach (var tag in selectedTags)
 			{
 				if (TagCategory.Qualification.Contains(tag))
 				{
-					employableOperators = employableOperators.Where(o => o.Rarity == QualificationTagToRarity(tag));
+					employableOperators = employableOperators.Where(o => o.Rarity == QualificationToRarity(tag));
 				}
 				else if (TagCategory.Class.Contains(tag))
 				{
@@ -37,17 +50,17 @@ namespace DontMissVulcan.Models
 				}
 				else
 				{
-					throw new ArgumentException($"タグ '{tag}' は有効なタグではありません。", nameof(tags));
+					throw new ArgumentException($"タグ '{tag}' は有効なタグではありません。", nameof(selectedTags));
 				}
 			}
 			return employableOperators;
 		}
 
-		private static int QualificationTagToRarity(Tag qualificationTag)
+		private static int QualificationToRarity(Tag qualificationTag)
 		{
 			if (!TagCategory.Qualification.Contains(qualificationTag))
 			{
-				throw new ArgumentException($"rarityTag '{qualificationTag}' は有効なレア度タグではありません。", nameof(qualificationTag));
+				throw new ArgumentException($"qualificationTag '{qualificationTag}' は有効なレア度タグではありません。", nameof(qualificationTag));
 			}
 			return qualificationTag switch
 			{
