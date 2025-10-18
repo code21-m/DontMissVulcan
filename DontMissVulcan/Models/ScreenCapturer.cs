209e868954b067557466bde73d688a022d0d19f2
code21-m <raspberryjam7modely@gmail.com>
2025-10-19 06:12:44 +0900

    ウィンドウのスクリーンショットを撮影する機能を実装

---
+++ DontMissVulcan/Models/ScreenCapturer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontMissVulcan.Models
+{
+	internal static class ScreenCapturer
+	{
+		public static Bitmap CaptureWindow(nint hWnd)
+		{
+			var rectangle = WindowHelper.GetWindowRectangle(hWnd);
+			return CaptureRectangle(rectangle);
+		}
+
+		public static Bitmap CaptureRectangle(Rectangle rectangle)
+		{
+			var bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+			using (var graphics = Graphics.FromImage(bitmap))
+			{
+				graphics.CopyFromScreen(rectangle.Left, rectangle.Top, 0, 0, rectangle.Size);
+			}
+			return bitmap;
+		}
+	}
+}
