346059b741347550693765d360cd2bb29311967a
code21-m <raspberryjam7modely@gmail.com>
2025-10-22 04:15:49 +0900

    タグ一覧の表示

---
+++ DontMissVulcan/ViewModels/TagItemViewModel.cs
@@ -0,0 +1,14 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using DontMissVulcan.Models.Domain;
+
+namespace DontMissVulcan.ViewModels
+{
+	public partial class TagItemViewModel(Tag tag, string displayName) : ObservableObject
+	{
+		public Tag Tag { get; } = tag;
+		public string DisplayName { get; } = displayName;
+
+		[ObservableProperty]
+		public partial bool IsSelected { get; set; }
+	}
+}
--- DontMissVulcan/ViewModels/TagSelectorViewModel.cs
+++ DontMissVulcan/ViewModels/TagSelectorViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DontMissVulcan.Models.Domain;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DontMissVulcan.ViewModels
 {
-	internal class TagSelectorViewModel
+	public partial class TagSelectorViewModel : ObservableObject
 	{
+		public ObservableCollection<TagCategoryViewModel> Categories { get; } = new();
+
+		public TagSelectorViewModel()
+		{
+			Categories.Add(new TagCategoryViewModel("Qualification", TagCategories.QualificationTags.Select(t => new TagItemViewModel(t, t.ToString()))));
+			Categories.Add(new TagCategoryViewModel("Class", TagCategories.ClassTags.Select(t => new TagItemViewModel(t, t.ToString()))));
+			Categories.Add(new TagCategoryViewModel("Position", TagCategories.PositionTags.Select(t => new TagItemViewModel(t, t.ToString()))));
+			Categories.Add(new TagCategoryViewModel("Specialization", TagCategories.SpecializationTags.Select(t => new TagItemViewModel(t, t.ToString()))));
+		}
 	}
 }
