87c5414f66c0f7116525184fe3c20fe60be55145
code21-m <raspberryjam7modely@gmail.com>
2025-10-22 04:18:26 +0900

    日本語コメントを削除

--- DontMissVulcan/Models/Domain/Tag.cs
+++ DontMissVulcan/Models/Domain/Tag.cs
@@ -3,40 +3,40 @@
 	public enum Tag
 	{
 		// Qualification
-		SeniorOperator, // 上級エリート
-		TopOperator,    // エリート
+		SeniorOperator,
+		TopOperator,
 
 		// Class
-		Vanguard,       // 先鋒タイプ
-		Guard,          // 前衛タイプ
-		Defender,       // 重装タイプ
-		Sniper,         // 狙撃タイプ
-		Caster,         // 術師タイプ
-		Medic,          // 医療タイプ
-		Supporter,      // 補助タイプ
-		Specialist,     // 特殊タイプ
+		Vanguard,
+		Guard,
+		Defender,
+		Sniper,
+		Caster,
+		Medic,
+		Supporter,
+		Specialist,
 
 		// Position
-		Melee,          // 近距離
-		Ranged,         // 遠距離
+		Melee,
+		Ranged,
 
 		// Specialization
-		AoE,            // 範囲攻撃
-		CrowdControl,   // 牽制
-		DPS,            // 火力
-		DPRecovery,     // COST回復
-		Debuff,         // 弱化
-		FastRedeploy,   // 高速再配置
-		Defense,        // 防御
-		Elemental,      // 元素
-		Healing,        // 治療
-		Nuker,          // 爆発力
-		Shift,          // 強制移動
-		Slow,           // 減速
-		Summon,         // 召喚
-		Support,        // 支援
-		Survival,       // 生存
-		Starter,        // 初期
-		Robot           // ロボット
+		AoE,
+		CrowdControl,
+		DPS,
+		DPRecovery,
+		Debuff,
+		FastRedeploy,
+		Defense,
+		Elemental,
+		Healing,
+		Nuker,
+		Shift,
+		Slow,
+		Summon,
+		Support,
+		Survival,
+		Starter,
+		Robot
 	}
 }
