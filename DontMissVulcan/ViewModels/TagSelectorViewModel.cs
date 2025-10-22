5ff44ecb256629366795a1fb4e6cf6fb5dfc37d8
code21-m <raspberryjam7modely@gmail.com>
2025-10-23 00:41:10 +0900

    タグの選択上限と現在選択されているタグのコレクションを実装

--- DontMissVulcan/ViewModels/TagSelectorViewModel.cs
+++ DontMissVulcan/ViewModels/TagSelectorViewModel.cs
@@ -2,12 +2,15 @@
 using DontMissVulcan.Models.Domain;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.ComponentModel;
 
 namespace DontMissVulcan.ViewModels
 {
 	public partial class TagSelectorViewModel : ObservableObject
 	{
+
 		public ObservableCollection<TagCategoryViewModel> Categories { get; } = [];
+		public ObservableCollection<Tag> SelectedTags { get; } = [];
 
 		public TagSelectorViewModel()
 		{
@@ -15,6 +18,48 @@ namespace DontMissVulcan.ViewModels
 			Categories.Add(new TagCategoryViewModel("Class", TagCategories.ClassTags.Select(t => new TagItemViewModel(t, t.ToString()))));
 			Categories.Add(new TagCategoryViewModel("Position", TagCategories.PositionTags.Select(t => new TagItemViewModel(t, t.ToString()))));
 			Categories.Add(new TagCategoryViewModel("Specialization", TagCategories.SpecializationTags.Select(t => new TagItemViewModel(t, t.ToString()))));
+
+			foreach (var category in Categories)
+			{
+				foreach (var tag in category.Tags)
+				{
+					tag.PropertyChanged += TagSelectionChanged;
+				}
+			}
+		}
+
+		private bool _isHandlingTagSelectionChange;
+		private void TagSelectionChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName != nameof(TagItemViewModel.IsSelected))
+			{
+				return;
+			}
+			if (_isHandlingTagSelectionChange)
+			{
+				return;
+			}
+			if (sender is not TagItemViewModel changed)
+			{
+				return;
+			}
+
+			if (changed.IsSelected)
+			{
+				var totalSelected = Categories.SelectMany(c => c.Tags).Count(t => t.IsSelected);
+				if (totalSelected > 5)
+				{
+					_isHandlingTagSelectionChange = true;
+					changed.IsSelected = false;
+					_isHandlingTagSelectionChange = false;
+					return;
+				}
+				SelectedTags.Add(changed.Tag);
+			}
+			else
+			{
+				SelectedTags.Remove(changed.Tag);
+			}
 		}
 	}
 }
