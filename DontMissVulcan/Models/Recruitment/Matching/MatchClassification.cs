0dfc6a0dbf69fddd5f30b5c6abd0cdd62eb2068c
code21-m <raspberryjam7modely@gmail.com>
2025-10-23 23:08:14 +0900

    命名変更

--- DontMissVulcan/Models/Recruitment/Matching/MatchClassification.cs
+++ DontMissVulcan/Models/Recruitment/Matching/MatchClassification.cs
@@ -4,7 +4,7 @@ namespace DontMissVulcan.Models.Recruitment.Matching
 {
 	public sealed record MatchClassification(
 			IReadOnlyCollection<Match> SixStarsOperators,
-			IReadOnlyCollection<Match> FiveStarsOrHigherOperators,
+			IReadOnlyCollection<Match> FiveStarsOperators,
 			IReadOnlyCollection<Match> FourStarsOrHigherOperators,
 			IReadOnlyCollection<Match> Robots
 		);
