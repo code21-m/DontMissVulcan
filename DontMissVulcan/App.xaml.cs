bf3d6106e7d503a23e63b2912b354bf5312df66f
code21-m <raspberryjam7modely@gmail.com>
2025-10-27 01:16:31 +0900

    コメントの記述 サービスプロバイダの破棄をプロセス終了時に変更

--- DontMissVulcan/App.xaml.cs
+++ DontMissVulcan/App.xaml.cs
@@ -2,35 +2,62 @@
 using DontMissVulcan.ViewModels.Recruitment;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
+using System;
 
 namespace DontMissVulcan
 {
+    /// <summary>
+    /// アプリケーション
+    /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// ウィンドウ
+        /// </summary>
         private Window? _window;
 
-        private readonly ServiceProvider _serviceProvider;
+		/// <summary>
+		/// サービスプロバイダ
+		/// </summary>
+		private readonly ServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// シングルトンオブジェクトを初期化します。
+        /// ここに書かれたコードが最初に実行されます。
+        /// </summary>
         public App()
         {
+            // あとで破棄するためにサービスプロバイダを保持しておく。
             _serviceProvider = new ServiceCollection()
                 .AddSingleton<RecruitmentViewModel>()
                 .BuildServiceProvider();
+
             Ioc.Default.ConfigureServices(_serviceProvider);
 
-            InitializeComponent();
+			// プロセス終了時にサービスプロバイダを破棄する。
+			AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
+			InitializeComponent();
         }
 
+        /// <summary>
+        /// アプリケーション起動時の処理を行います。
+        /// </summary>
+        /// <param name="args"></param>
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             _window = new MainWindow();
-            _window.Closed += WindowClosed;
             _window.Activate();
         }
 
-        private void WindowClosed(object? sender, WindowEventArgs e)
-        {
-            _serviceProvider.Dispose();
-        }
+        /// <summary>
+        /// プロセス終了時にサービスプロバイダを破棄します。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+		private void OnProcessExit(object? sender, EventArgs e)
+		{
+			_serviceProvider.Dispose();
+		}
     }
 }
--- DontMissVulcan/Views/Recruitment/RecruitmentView.xaml.cs
+++ DontMissVulcan/Views/Recruitment/RecruitmentView.xaml.cs
@@ -4,12 +4,22 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace DontMissVulcan.Views.Recruitment
 {
+	/// <summary>
+	/// 公開求人ツールのView
+	/// </summary>
 	public sealed partial class RecruitmentView : Page
 	{
+		/// <summary>
+		/// 公開求人ツールのViewModel
+		/// </summary>
 		internal RecruitmentViewModel ViewModel { get; }
 
+		/// <summary>
+		/// Viewの初期化を行います。
+		/// </summary>
 		public RecruitmentView()
 		{
+			// DIコンテナから公開求人ツールのViewModelを取得する。
 			ViewModel = Ioc.Default.GetRequiredService<RecruitmentViewModel>();
 
 			InitializeComponent();
